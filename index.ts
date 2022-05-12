5e38e2a00634a1cdef85d0adcf0bd1e011f1be1e
nhack <cristea12@gmail.com>
2022-05-12 15:52:44 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,20 +1,23 @@
-import { interval } from 'rxjs';
+import { interval, Subject } from 'rxjs';
 import { take } from 'rxjs/operators';
 
 const source$ = interval(1000).pipe(take(4));
+const subject$ = new Subject();
 
-source$.subscribe((value) => console.log(`Observer 1: ${value}`));
+source$.subscribe(subject$);
+
+subject$.subscribe((value) => console.log(`Observer 1: ${value}`));
 setTimeout(
-  () => source$.subscribe((value) => console.log(`Observer 2: ${value}`)),
+  () => subject$.subscribe((value) => console.log(`Observer 2: ${value}`)),
   1000
 );
 setTimeout(
-  () => source$.subscribe((value) => console.log(`Observer 3: ${value}`)),
+  () => subject$.subscribe((value) => console.log(`Observer 3: ${value}`)),
   2000
 );
 setTimeout(
   () =>
-    source$.subscribe({
+    subject$.subscribe({
       next: (value) => console.log(`Observer 4: ${value}`),
       complete: () => console.log('Observer 4 complete.'),
     }),
