1fd5a82011497e4b2f52a3868e735cec5ac3ef3e
nhack <cristea12@gmail.com>
2022-05-12 16:11:04 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,29 +1,42 @@
+import { ConnectableObservable, interval, Subject } from 'rxjs';
 import {
-  queueScheduler,
-  asapScheduler,
-  asyncScheduler,
-  of,
-  merge,
-  from,
-  scheduled,
-} from 'rxjs';
+  multicast,
+  publish,
+  publishBehavior,
+  publishLast,
+  publishReplay,
+  refCount,
+  share,
+  take,
+} from 'rxjs/operators';
 
-import { tap, observeOn } from 'rxjs/operators';
+const source$ = interval(1000).pipe(
+  take(4)
+  //multicast(new Subject()),
+  //publish(),
+  //publishLast(),
+  //publishBehavior(66),
+  //publishReplay(),
+  //refCount()
+  //share()
+) as ConnectableObservable<number>;
 
-console.log('Start script.');
+source$.subscribe((value) => console.log(`Observer 1: ${value}`));
+setTimeout(
+  () => source$.subscribe((value) => console.log(`Observer 2: ${value}`)),
+  1500
+);
+setTimeout(
+  () => source$.subscribe((value) => console.log(`Observer 3: ${value}`)),
+  2500
+);
+setTimeout(
+  () =>
+    source$.subscribe({
+      next: (value) => console.log(`Observer 4: ${value}`),
+      complete: () => console.log('Observer 4 complete.'),
+    }),
+  4500
+);
 
-const queue$ = scheduled(['QueueScheduler (sync task)'], queueScheduler);
-const asap$ = scheduled(['AsapScheduler (async micro task)'], asapScheduler);
-const async$ = scheduled(['AsyncScheduler (async task)'], asyncScheduler);
-
-merge(queue$, asap$, async$).subscribe((value) => console.log(value));
-
-scheduled([1, 2, 3, 4], queueScheduler)
-  .pipe(
-    tap((value) => console.log(`Value: ${value}`)),
-    observeOn(asyncScheduler),
-    tap((value) => console.log(`Doubled value: ${value * 2}`))
-  )
-  .subscribe();
-
-console.log('End script.');
+//source$.connect();
