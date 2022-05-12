dc7c8837512531e8672fccf2c09ded3d46809a47
nhack <cristea12@gmail.com>
2022-05-12 14:47:41 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,12 +1,24 @@
-import { Observable, OperatorFunction } from 'rxjs';
-import { map } from 'rxjs/operators';
-
+import { Observable, fromEvent, OperatorFunction } from 'rxjs';
+import { map, filter, takeUntil } from 'rxjs/operators';
 const currentTime$: Observable<Date> = new Observable((subscriber) => {
   setInterval(() => subscriber.next(new Date()), 1000);
 });
 
-const toTime = (date: Date) => date.getTime();
-const toTimeMap: OperatorFunction<Date, number> = map(toTime);
+const btn = document.getElementById('btn');
+const stop$ = fromEvent(btn, 'click');
 
-toTimeMap(currentTime$).subscribe((time) => console.log('Manual:' + time));
-currentTime$.pipe(toTimeMap).subscribe((time) => console.log('Pipe:' + time));
+const getEvenTimestamps: (
+  stop$: Observable<any>
+) => OperatorFunction<Date, number> = (stop$: Observable<any>) => {
+  return (source$: Observable<Date>) =>
+    source$.pipe(
+      map((date) => date.getTime()),
+      filter((time) => time % 2 === 0),
+      takeUntil(stop$)
+    );
+};
+
+currentTime$.pipe(getEvenTimestamps(stop$)).subscribe({
+  next: (time) => console.log(time),
+  complete: () => console.log('DONE'),
+});
